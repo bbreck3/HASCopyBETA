7bf7e561c6cc6ebdbeecd99e84a4c39ae274a1ed
bbreck3 <bbreck3@gmail.com>
2015-01-30 20:44:52 -0500

    Text file working and background implimented

---
+++ HASCopyBETA/HASCopyBETA.Designer.vb
@@ -0,0 +1,204 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class HASCopyBETA
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 25)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(116, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Select Source"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 54)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(116, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Select Destination"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(545, 136)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Start"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(626, 136)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Reset"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 107)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(689, 23)
+        Me.ProgressBar1.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(134, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(78, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "None Selected"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(134, 59)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(78, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "None Selected"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 91)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Copying: "
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(59, 91)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(16, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "..."
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(606, 91)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(13, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "0"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(639, 91)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(62, 13)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "% Complete"
+        '
+        'Timer1
+        '
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(12, 9)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(85, 13)
+        Me.Label7.TabIndex = 11
+        Me.Label7.Text = "Machine Name: "
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(134, 9)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(33, 13)
+        Me.Label8.TabIndex = 12
+        Me.Label8.Text = "None"
+        '
+        'BackgroundWorker1
+        '
+        '
+        'HASCopyBETA
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(708, 164)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "HASCopyBETA"
+        Me.Text = "HASCopyBETA"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+
+End Class
---
+++ HASCopyBETA/HASCopyBETA.vb
@@ -0,0 +1,355 @@
+﻿Option Explicit On
+Imports System.IO
+Imports System.Text
+Imports System.ComponentModel
+
+Public Class HASCopyBETA
+
+    Dim source As String
+    Dim target As String
+    Dim sourcePath As String '
+    Dim targetPath As String
+    Dim file1 As String
+
+    ' Dim numFiles As  _
+    '    System.Collections.ObjectModel.ReadOnlyCollection(Of String)
+    Dim source_list As New List(Of String)
+    Dim target_list As New List(Of String)
+    Dim files() As String
+
+    Dim fileName As String
+    Dim fileNames As String
+    ' Dim Form2 As New Form2
+    Dim filesTransferred As Integer = 0
+    Dim fso = CreateObject("Scripting.FileSystemObject")
+    Dim srclist As List(Of String)
+    Dim Trglist As List(Of String)
+    Dim src As Integer
+    Dim tot_prog As Double
+
+    Dim trg_dir
+    Dim src_dir
+    Dim source_size
+    Dim target_size
+    Private Property WScript As Object
+    Dim target_tot = 0
+    Dim temp = 0
+    Dim machine_name = My.Computer.Name
+    Dim filepath As String
+    Dim fs As FileStream
+    Dim writer As StreamWriter
+    Dim filereader As StreamReader
+    Dim dir_exc As String
+    Dim list As String
+    Private Sub Source_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim sourceDialog As FolderBrowserDialog = New FolderBrowserDialog
+        Dim openFileDialog1 As OpenFileDialog = New OpenFileDialog
+
+        If sourceDialog.ShowDialog() = DialogResult.OK Then
+            Label1.Text = sourceDialog.SelectedPath
+            source = sourceDialog.SelectedPath
+            source_size = CountFiles(source, 0.0)
+        End If
+        If DialogResult.Cancel Then
+            Return
+        End If
+
+
+
+        '   source_size = CountFiles(source, 0.0)
+        ' DirectorySize(source, True)
+
+
+    End Sub
+
+    Private Sub Destionation_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim openFileDialog1 As OpenFileDialog = New OpenFileDialog
+
+        Dim targetDialog As FolderBrowserDialog = New FolderBrowserDialog
+        If targetDialog.ShowDialog() = DialogResult.OK Then
+            Label2.Text = targetDialog.SelectedPath
+            target = targetDialog.SelectedPath
+            filepath = target + "/" + machine_name + "_ERROR_LOG.txt"
+            writer = New StreamWriter(filepath, True, System.Text.Encoding.ASCII)
+            'fs = File.Create(filepath)
+            'Dim info As Byte() = New UTF8Encoding(True).GetBytes("This is a test")
+            'fs.Write(info, 0, info.Length)
+            'fs.Close()
+        End If
+        If DialogResult.Cancel Then
+            Return
+        End If
+
+
+
+        ' Dim file_writer As New System.IO.StreamWriter(filepath)
+        'file_writer.WriteLine("This is a test")
+        'target_size = DirectorySize(target, True)
+
+
+    End Sub
+
+    Private Sub Start_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ' Process.Start("CMD", "/c cd  " + source + " & start cmd.exe /c Robocopy " + """" + source + """" + " " + """" + target + """" + " /S") '" /LOG+:log.txt" + " /S")
+        'target_size = DirectorySize(target, True)
+        'While target_size IsNot source_size
+        ' temp = target_size
+        'target_tot = target_tot + temp
+        ' tot_prog = Math.Round(target_size)
+
+        'temp = 0
+        'target_size = DirectorySize(target, True)
+        'tot_prog = Math.Round(target_size / source_size)
+        'Label5.Text = tot_prog.ToString()
+        'Label5.Refresh()
+        'End While
+
+
+        'ProgressBar1.Style = ProgressBarStyle.Continuous
+
+
+        'bw.RunWorkerAsync()
+        BackgroundWorker1.RunWorkerAsync()
+
+        ' CopyDirectory(source, target)
+        ' My.Computer.FileSystem.CopyFile(source)
+
+
+
+
+
+
+
+
+    End Sub
+
+   
+
+    Private Sub Reset_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        Label1.Text = "None Specified"
+        Label2.Text = "None Specified"
+        ProgressBar1.Value = 0
+
+
+        target_size = 0
+
+
+
+        Label5.Text = target_size.ToString()
+
+        Label5.Refresh()
+        Label4.Text = "..."
+        Label4.Refresh()
+
+
+
+
+
+
+    End Sub
+
+    Private Sub ProgressBar1_Click(sender As Object, e As EventArgs) Handles ProgressBar1.Click
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+
+        'trg_dir = New IO.DirectoryInfo(target)
+        'src_dir = New IO.DirectoryInfo(source)
+        Try
+
+
+            'tot_prog = (trg_dir.GetFiles.Length / src_dir.GetFiles.Length)
+
+
+
+
+        Catch ex As Exception
+            ' MsgBox(src_dir.FullName())
+
+
+        End Try
+        ' ProgressBar1.Increment(tot_prog)
+
+
+        'Label5.Text = ProgressBar1.Value
+
+
+        ' Label5.Refresh()
+
+
+        If ProgressBar1.Value = 100 Then
+            Timer1.Stop()
+            'Form2.Show()
+
+        End If
+    End Sub
+
+
+
+
+
+
+
+
+
+
+
+    Private Sub CopyDirectory(ByVal sourcePath As String, ByVal destPath As String)
+
+        'Does not work correctly ...
+        '  If Label1.Text = "None Selected" Or Label2.Text = "None Selected" Then
+        'MsgBox("Source or Destination not specified. \n Please specify a Source and a Destination.")
+
+        ' End If
+
+        If Not Directory.Exists(destPath) Then
+            Directory.CreateDirectory(destPath)
+        End If
+        Try
+            For Each file1 As String In Directory.GetFiles(sourcePath)
+                ' Label4.Text = file1.ToString
+                'Label4.Refresh()
+
+
+
+                Dim dest As String = Path.Combine(destPath, Path.GetFileName(file1))
+                File.Copy(file1, dest, True)  ' Added True here to force the an overwrite
+                target_size = CountFiles(target, 0.0)
+                Label4.Text = file1.ToString
+                Label4.Refresh()
+
+                ' My.Computer.FileSystem.CopyFile(file1, dest, FileIO.UIOption.AllDialogs)
+                ' target_size = DirectorySize(destPath, True)
+                'temp = target_size
+                '  target_tot = target_tot + temp
+
+
+                tot_prog = Math.Round((target_size / source_size), 2) * 100
+                ProgressBar1.Value = tot_prog
+                ' Dim test = ProgressBar1.Value
+
+                Label5.Text = tot_prog.ToString()
+                Label5.Refresh()
+
+            Next
+
+            ' Use directly the sourcePath passed in, not the parent of that path
+            For Each dir1 As String In Directory.GetDirectories(sourcePath)
+                dir_exc = dir1
+                Dim destdir As String = Path.Combine(destPath, Path.GetFileName(dir1))
+                CopyDirectory(dir1, destdir)
+            Next
+            'Attempts to catch the error from any issue in copy a file ands it to a Text File
+            'currently does not work
+        Catch e As Exception
+
+            list = list + dir_exc + Environment.NewLine
+            writer.WriteLine(list)
+
+
+
+            'My.Application.Log.WriteException(e, TraceEventType.Error, filepath & ".")
+            ' IO.File.AppendAllText(filepath, String.Format("{0}{1}", Environment.NewLine, e.ToString()))
+
+
+
+
+            'MsgBox(e)
+            'Dim error_file As Byte() = New UTF8Encoding(True).GetBytes(file1.ToString())
+            'fs.Write(error_file, 0, error_file.Length)
+            ' fs.Close()
+
+        End Try
+
+    End Sub
+    Private Function CountFiles(InFolder As String, ByRef Result As Double)
+        Result += IO.Directory.GetFiles(InFolder).Count
+        Dim temp = ProgressBar1.Value
+
+        For Each f As String In IO.Directory.GetDirectories(InFolder)
+            CountFiles(f, Result)
+        Next
+        If temp = 15 Then
+
+            writer.Close()
+        End If
+        'This is a problem...once the prog bar hits 100 percent it stays there and there there is an infinite msgbox loop
+        'MsgBox("Copy Complete!")
+        'Return Result
+        'End If
+
+
+        Return Result
+
+    End Function
+
+
+
+
+
+    Private Function DirectorySize(ByVal sPath As String, ByVal bRecursive As Boolean) As Double
+
+        'Dim lngNumberOfDirectories As Long
+        Dim lngNumberOfDirectories As Double
+        Dim Size As Long = 0
+
+        Dim diDir As New DirectoryInfo(sPath)
+
+        Try
+
+            Dim fil As FileInfo
+
+            For Each fil In diDir.GetFiles()
+
+                Size += fil.Length
+
+            Next fil
+
+            If bRecursive = True Then
+
+                Dim diSubDir As DirectoryInfo
+
+                For Each diSubDir In diDir.GetDirectories()
+
+                    Size += DirectorySize(diSubDir.FullName, True)
+
+                    lngNumberOfDirectories += 1
+
+                Next diSubDir
+
+            End If
+
+            Return Size
+
+        Catch ex As System.IO.FileNotFoundException
+
+            ' File not found. Take no action
+
+        Catch exx As Exception
+
+            ' Another error occurred
+
+            Return 0
+
+        End Try
+        Return Size
+    End Function
+
+    Protected Overrides Sub OnLoad(ByVal e As EventArgs)
+        MyBase.OnLoad(e)
+
+        Label8.Text = machine_name
+
+
+        '  Dim string_to_write As New StringBuilder()
+        ' For Each txtName As String In Directory
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        CopyDirectory(source, target)
+        Timer1.Start()
+    End Sub
+End Class
